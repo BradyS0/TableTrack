676a654d1d46934426537b75e17fe9cbc3d57ac2
Braydon Schick <starwarsboi77@gmail.com>
2025-10-10 10:28:49 -0500

    trying to get int test working

--- source/backend/db.js
+++ source/backend/db.js
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER || "user",
     process.env.DB_PASS || "password",
     {
-        host: process.env.DB_HOST || "localhost",
+        host: 'db',
         dialect: "postgres",
         logging: false,
     }
