c5ba91b95dc5e4168f1ca76fb912248d17e617e4
Braydon Schick <starwarsboi77@gmail.com>
2025-10-10 01:05:25 -0500

    fix db setup

--- source/backend/db.js
+++ source/backend/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST || "localhost", // Database host
-        dialect: '"postgres'
+        dialect: 'postgres'
     }
 );
 
