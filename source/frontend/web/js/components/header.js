5c08b2b6d87c8fb3dd46f5cfc3859bf011ec579a
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-10-11 19:49:01 -0500

    hamburger menu component created

--- source/frontend/web/js/components/header.js
+++ source/frontend/web/js/components/header.js
@@ -1,9 +1,24 @@
 export function createHeader() {
   const header = document.createElement('header');
+  const nav = document.querySelector('nav');
+  const hamburger = document.createElement('h1');
+  const overlay = document.getElementById("overlay");
+  
+  hamburger.id = "hamburger"
+  hamburger.innerHTML = "&#9776;"
+  hamburger.addEventListener('click',()=>{
+    console.log("hamburger!!")
+    overlay.classList.add("overlay-active");
+    nav.classList.add('nav-active')
+  })
+
+  
   header.innerHTML = `
-    <image src="../assets/TableTrack-logo.svg" alt="Logo" class="logo"/>
-    <h1>TableTrack</h1>
-    <button class="login-btn">Login</button>
+  <image src="../assets/TableTrack-logo.svg" alt="Logo" class="logo"/>
+  <h1>Table<span>Track</span></h1>
+  <button class="login-btn">Login</button>
   `;
+
+  header.prepend(hamburger)
   return header;
 }
--- source/frontend/web/js/components/nav.js
+++ source/frontend/web/js/components/nav.js
@@ -1,5 +1,25 @@
-export function createNav() {
-  const nav = document.createElement('nav');
-  nav.innerHTML = '<a href="#">Menu</a>';
-  return nav;
+export function mainNavRoutes(){
+
+}
+
+export function createNav(menuItems = [item1, item2]) {
+  const nav = document.querySelector('nav')
+  const nav_close = document.getElementById("overlay");
+
+  nav_close.addEventListener('click', ()=>{
+     nav.classList.remove('nav-active')
+     nav_close.classList.remove('overlay-active')
+})
+
+  menuItems.forEach(item => {
+    const link = document.createElement('p');
+    link.innerText = item
+    nav.appendChild(link)
+  });
+
+  const merchantNote = document.createElement('div');
+  merchantNote.classList.add('merchant-note');
+  merchantNote.textContent = 'Become a merchant!';
+
+  nav.appendChild(merchantNote)
 }
