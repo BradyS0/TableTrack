4a822a0fddeb4b795ecd829b7b590d50c9cc2f5c
shaiynee <85891120+shaiynee@users.noreply.github.com>
2025-10-11 20:09:17 -0500

    added regex pattern to validate emails in logic/userLogic.js taken from https://mailtrap.io/blog/javascript-email-validation

--- source/backend/logic/userLogic.js
+++ source/backend/logic/userLogic.js
@@ -1,8 +1,7 @@
 function validate_email(email) {
-    if(email.includes("@"))
-        return true;
-    else
-        return false;
+    //email validation regex taken from https://mailtrap.io/blog/javascript-email-validation
+    const email_regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return email_regex.test(email);
 }
 
 function hash_password(password) {
