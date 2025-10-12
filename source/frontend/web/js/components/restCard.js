b9554198a1242e5bfc9c5a53e293114c256b85f3
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-10-12 17:49:46 -0500

    linked restaurant cards to restaruant-detail-page

--- source/frontend/web/js/components/restCard.js
+++ source/frontend/web/js/components/restCard.js
@@ -1,6 +1,6 @@
 const tempImagesrc = "https://media.istockphoto.com/id/1038356020/vector/restaurant-icon.jpg?s=612x612&w=0&k=20&c=Tk_v3JuJA4lz_8ZRJi78xS4p75Idqt97uEtYJciVtFI=" 
 const timeImage = "../assets/time.svg"
-export function createCard({ restID, name, logo, tags, hours, rating }) {
+export function createCard({ restId, name, logo, tags, hours, rating }) {
   const card = document.createElement('div');
   card.classList.add('restCard');
   const tagsHTML = tags.map(tag => `<p>${tag}</p>`).join('');
@@ -11,6 +11,11 @@ export function createCard({ restID, name, logo, tags, hours, rating }) {
     <span class="restTime"> <image src="${timeImage}"/> ${hours || ''}</span>
     ${rating ? `<span class="rating">★ ${rating}</span>` : ''}
   `;
+
+  card.addEventListener('click', () => {
+    window.location.href = `./restaurantDetail.html?restId=${restId}`;
+  })
+
   return card;
 }
 
