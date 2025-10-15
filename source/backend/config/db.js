d37bd175676aab4739772b1e1263ff3e3a8b4fa4
Matt-Byk <boychu13@myumanitoba.ca>
2025-10-15 12:30:38 -0500

    separated sequelize and config imports in index

--- source/backend/config/db.js
+++ source/backend/config/db.js
@@ -50,4 +50,5 @@ if (process.env.NODE_ENV !== 'production') {
         });
 }
 
-module.exports = {sequelize};
+export default sequelize;
+//module.exports = {sequelize};
--- source/backend/index.js
+++ source/backend/index.js
@@ -3,8 +3,8 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.js";
 import restaurantRouter from "./routes/restaurantRouter.js";
-// import sequelize from "./db.js";
-import sequelize, { config } from "./config/config.js";
+import sequelize from "./config/db.js";
+import { config } from "./config/config.js";
 
 const app = express();
 
