a2c40cf39c509bf91d2432ed31715907ac730dde
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-10-14 22:19:31 -0500

    created a popup msg component

--- source/frontend/web/js/components/nav.js
+++ source/frontend/web/js/components/nav.js
@@ -1,6 +1,23 @@
 export function mainNavRoutes(){
+   //if user logged-in
+    //show user-profile option
+    //show restaurant management option - redirect to restaurant management page
+    //show a logout button
+
+  //else show a signin button
+
+  //------ hr ---------
+  //Home
+  //About - working in progress popup
+  //Become a merchant
+      // if user not logged in - redirect to login-page show a popup that user needs to be logged-in to become a merchant
+      // if user logged in show become merchantRegistration popup
+      // if user already a merchant dont show the option
+
   return null;
 }
+
+
 
 export function createNav(menuItems = [item1, item2]) {
   const nav = document.querySelector('nav')
