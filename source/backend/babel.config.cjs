a98208500b84fc701b07e7c80f5df35be79a3bdf
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-10-15 16:25:42 -0500

    Revert "CI additions"
    This reverts commit 69d320a37494755238accc5de84e0f9e3343c01d.

--- source/backend/babel.config.cjs
+++
@@ -1,8 +0,0 @@
-﻿// made using chatGPT
-// this file translates modern JS to older JS for compatibility
-// really only used for jest testing with ES modules
-module.exports = {
-    presets: [
-        ["@babel/preset-env", { targets: { node: "current" } }]
-    ]
-};
--- source/backend/logic/userLogic.js
+++
@@ -1,8 +0,0 @@
-﻿function validate_email(email) {
-    if (email.includes("@"))
-        return true;
-    else
-        return false;
-}
-
-module.exports = { validate_email };
