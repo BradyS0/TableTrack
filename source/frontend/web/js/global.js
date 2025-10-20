7c4fbe7d70de395c25f4ea03d9981b38eac92841
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-10-20 15:04:53 -0500

    created mockAPIcalls for users

--- source/frontend/web/js/global.js
+++ source/frontend/web/js/global.js
@@ -1,7 +1,11 @@
 import { createHeader } from './components/header.js';
 import { getUserState, setUserState } from './utils.js';
+import {mockUsersAPI} from './api_calls/mock/user_api.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
   app.appendChild(createHeader());
+
+  let result = mockUsersAPI.createUser("Arsalan","Siddiqui","arsalan@gmail.com","newPassword1");
+  console.log(result)
 });
