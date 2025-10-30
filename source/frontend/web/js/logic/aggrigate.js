744895e07907a2ec2c8303349f66b272f3e1cf50
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-10-30 13:03:45 -0500

    created search aggrigation function

---
+++ source/frontend/web/js/logic/aggrigate.js
@@ -0,0 +1,10 @@
+import {restaurants} from "../api_calls/mock/mockRestdata.js"
+
+export function restaurantSearchByNameTags(search_by, restaurants){
+    const result = restaurants.filter(rest =>
+        rest.name.toLowerCase().includes(search_by.toLowerCase())
+        || rest.tags.filter(tag => tag.toLowerCase().includes(search_by.toLowerCase())).length > 0
+    );
+
+    return result
+}
