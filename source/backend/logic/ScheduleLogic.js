e0814985e38d6e03448bb32a99db3ec05b8b3d85
Ratia5 <129026847+Ratia5@users.noreply.github.com>
2025-11-03 11:39:57 -0600

    Fixes2
    append to push, regex changes

--- source/backend/logic/ScheduleLogic.js
+++ source/backend/logic/ScheduleLogic.js
@@ -14,9 +14,9 @@ function validate_schedule(input)
 {
     // Create regular expression to check formatting of the schedule
     const day_regex  = "(sunday|monday|tuesday|wednesday|thursday|friday|saturday)";
-    const time_regex = "([0-9]{1}\\.[0-9]{2}|1[0-9]{1}\\.[0-9]{2}|2[0-3]{1}\\.[0-9]{1}|24\\.00)";
+    const time_regex = "([0-9]{1}\\.[0-9]{1,2}|1[0-9]{1}\\.[0-9]{1,2}|2[0-3]{1}\\.[0-9]{1,2}|24\\.(0|00))";
     const line_regex = "\""+day_regex+"\":\\{\"open\":"+time_regex+", \"close\":"+time_regex+"\\}";
-    const list_regex = "\\{\"schedule\":\\{("+line_regex+"(, ){0,1}){0,7}\\}\\}";
+    const list_regex = "^\\{\"schedule\":\\{("+line_regex+"(, ){0,1}){0,7}\\}\\}$";
 
     // Compare input to regex
     const reg = new RegExp(list_regex);
@@ -30,23 +30,23 @@ function parse_schedule(input)
 {
     // Get each day from schedule
     var days = [];
-    days.append(input.sunday);
-    days.append(input.monday);
-    days.append(input.tuesday);
-    days.append(input.wednesday);
-    days.append(input.thursday);
-    days.append(input.friday);
-    days.append(input.saturday);
+    days.push(input.sunday);
+    days.push(input.monday);
+    days.push(input.tuesday);
+    days.push(input.wednesday);
+    days.push(input.thursday);
+    days.push(input.friday);
+    days.push(input.saturday);
 
     // Loop through days and add to schedule
     var schedule = [];
     for (var i = 0; i < days.length; i++)
     {
         // CASE 1: Undefined, no schedule given
-        if (days[i] === undefined) schedule.append([0.0, 0.0]);
+        if (days[i] === undefined) schedule.push([0.0, 0.0]);
         
         // CASE 2: Defined, Set to given schedule
-        else schedule.append(days[i].open, days[i].close);
+        else schedule.push(days[i].open, days[i].close);
     }
     return schedule;
 }
