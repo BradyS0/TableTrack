f38fe24a917ace9c91486a8f4976dda86f4a1a9a
arsalan2708 <108593475+arsalan2708@users.noreply.github.com>
2025-11-03 17:05:40 -0600

    backend-api  adding tags to restaurant (#75)
    * added tags to the Restaurant model, added tags validation logic, added tags to creation logic and finally added patch route for restaurant tags
    
    * Updated Restaurant Tags in the model to accept array of strings, improved some regex validation for restaurant tags, created UT and IT related to Restaurant Tags
    
    * fixing a typo in restaurant it
    
    * Update IT for restaurant tags

--- source/backend/logic/restaurantLogic.js
+++ source/backend/logic/restaurantLogic.js
@@ -73,6 +73,24 @@ function validate_hours(hours) {
     return regex.test(hours);
 }
 
+function validate_tags(tags){
+    const MIN_LEN = 3
+    const MAX_LEN = 30
+    //regex only accepts hyphens and alphabets , cannopt start or end with a hyphen
+    const tags_regex = /^[A-Za-z]+(?:-[A-Za-z]+)*$/
+
+    if(!Array.isArray(tags))
+        return false
+
+    const isValid = tags.every(tag =>
+        typeof(tag) === 'string' && 
+        tag.length>=MIN_LEN &&
+        tag.length<=MAX_LEN && 
+        tags_regex.test(tag));
+
+    return isValid
+}
+
 
 
 // Export functions
@@ -81,5 +99,6 @@ export default {
     validate_phone,
     validate_name,
     validate_description,
-    validate_hours
+    validate_hours,
+    validate_tags
 };
