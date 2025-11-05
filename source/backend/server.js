9215d695088b115ed56a2493953fa15396b026ac
BradyS0 <starwarsboi77@gmail.com>
2025-11-05 09:05:40 -0600

    add clinic profiler

--- source/backend/server.js
+++ source/backend/server.js
@@ -1,7 +1,7 @@
 import { app } from "./app.js";
 import sequelize from "./db.js";
 
-const PORT = 3000;
+const PORT = process.env.API_PORT || 3001;
 
 const startServer = () => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- source/backend/testAllEndpoints.js
+++ source/backend/testAllEndpoints.js
@@ -7,11 +7,16 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-const API_PORT = process.env.API_PORT || 3000;
+const API_PORT = process.env.API_PORT || 3001;
 const BASE_URL = 'http://localhost:'+API_PORT;
+
+const sched_wed =  {wednesday: { open: 9, close: 18.3 }}
+const sched_sun =  {sunday: { open: 0, close: 0 }}
+const sched_mon = {monday: { open: 14, close: 20.75 }}
 
 // Define all endpoints and their configurations
 const ENDPOINTS = [
+    // ----------------- User -----------------------
     {
         name: 'Create User',
         method: 'POST',
@@ -68,6 +73,173 @@ const ENDPOINTS = [
             userID: 1, // Replace with a valid userID
             email: 'janesmith@example.com',
         },
+    },
+    // ----------------- Restaurant -----------------------
+    {
+        name: 'Create Restaurant',
+        method: 'POST',
+        path: '/v1/restaurant',
+        body: {
+            userID: 1,
+            name: 'Burger Queen',
+            address: '100 Burger street',
+            phone:   '(204) 234-5678',
+            tags: ['new-restaurant','tester']
+        },
+    },
+    {
+        name: 'Get Restaurants',
+        method: 'GET',
+        path: '/v1/restaurant',
+    },
+    {
+        name: 'Get Restaurant By ID',
+        method: 'GET',
+        path: '/v1/restaurant/1',
+    },
+    {
+        name: 'Get Restaurant By User ID',
+        method: 'GET',
+        path: '/v1/restaurant/user/1',
+    },
+    {
+        name: 'Change Restaurant Info',
+        method: 'PATCH',
+        path: '/v1/restaurant/change',
+        body: {
+            restID: 1,
+            name: 'Burger Queen Updated',
+            address: '200 New Burger street',
+            phone:   '(204) 876-5432',
+            desc: 'An updated description for Burger Queen.',
+        },
+    },
+    {
+        name: 'Change Restaurant Description',
+        method: 'PATCH',
+        path: '/v1/restaurant/change/description',
+        body: {
+            restID: 1,
+            description: 'A new description for Burger Queen.',
+        },
+    },
+    {
+        name: 'Change Restaurant Tags',
+        method: 'PATCH',
+        path: '/v1/restaurant/change/tags',
+        body: {
+            restID: 1,
+            tags: ['updated-tagA', 'updated-tagB'],
+        },
+    },
+    {
+        name: 'Change Restaurant Name',
+        method: 'PATCH',
+        path: '/v1/restaurant/change/name',
+        body: {
+            restID: 1,
+            name: 'Burger Queen Renamed',
+        },
+    },
+    {
+        name: 'Change Restaurant Phone',
+        method: 'PATCH',
+        path: '/v1/restaurant/change/phone',
+        body: {
+            restID: 1,
+            phone: '(204) 999-8888',
+        },
+    },
+    {
+        name: 'Change Restaurant Address',
+        method: 'PATCH',
+        path: '/v1/restaurant/change/address',
+        body: {
+            restID: 1,
+            address: '300 Another Burger street',
+        },
+    },
+   // ----------------- Schedule -----------------------
+    {
+        name: 'Change Restaurant Schedule',
+        method: 'PUT',
+        path: '/v1/restaurant/schedule',
+        body: {
+            restID: 1,
+            schedule: sched_wed,
+        },
+    },
+    {
+        name: 'Get Restaurant Schedule',
+        method: 'GET',
+        path: '/v1/restaurant/schedule',
+        body: {
+            restID: 1,
+            day: 'wednesday',
+        },
+    },
+    // ----------------- Menu -----------------------
+    {
+        name: 'Create Menu Item',
+        method: 'POST',
+        path: '/v1/menu/1',
+        body: {
+            name: "Food",
+            price: "1.00",
+            description: "description",
+            category: "category"
+        },
+    },
+    {
+        name: 'Get Menu Items',
+        method: 'GET',
+        path: '/v1/menu/1',
+    },
+    {
+        name: 'Get Menu Item',
+        method: 'GET',
+        path: '/v1/menu/1/1',
+    },
+    {
+        name: 'Change Menu Item Name',
+        method: 'PATCH',
+        path: '/v1/menu/1/change/name',
+        body: {
+            itemID: 1,
+            name: "Newname",
+        },
+    },
+    {
+        name: 'Change Menu Item Price',
+        method: 'PATCH',
+        path: '/v1/menu/1/change/price',
+        body: {
+            itemID: 1,
+            price: "2.00",
+        },
+    },
+    {
+        name: 'Change Menu Item Description',
+        method: 'PATCH',
+        path: '/v1/menu/1/change/description',
+        body: {
+            itemID: 1,
+            description: "New description",
+        },
+    },
+    {
+        name: 'Change Menu Item Category',
+        method: 'PATCH',
+        path: '/v1/menu/1/change/category',
+        body: {
+            itemID: 1,
+            category: "NewCategory",
+        },
+    },
+    {
+        name: 'Delete Menu Item',
+        method: 'DELETE',
+        path: '/v1/menu/1/1',
     },
     {
         name: 'Delete User',
