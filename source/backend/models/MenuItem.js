b162654cf320c483cca495db679e1c2214947d78
Matt-Byk <boychu13@myumanitoba.ca>
2025-11-22 21:27:18 -0600

    Queries to data layer

--- source/backend/models/MenuItem.js
+++ source/backend/models/MenuItem.js
@@ -12,3 +12,117 @@ export const MenuItem = sequelize.define("MenuItem", {
     description: { type: DataTypes.TEXT },
     category:    { type: DataTypes.STRING },
 });
+
+// Query: Create a new menu item
+async function create(restID, name, price, desc, category)
+{
+    try{ // Attempt to create new menu item
+
+        return await MenuItem.create({
+            restID: restID,
+            name: name,
+            price: price,
+            description: desc,
+            category: category
+        });
+
+    }catch{ // Failed to create new menu item
+
+        throw new Error("Failed to create new Menu Item, verify the information provided is correct.");
+    }
+}
+
+// Query: Get all items from restaurant
+async function get_by_restaurant(restID)
+{
+    return await MenuItem.findAll({
+        attributes: [
+            "name",
+            "price",
+            "description",
+            "category"
+        ], where: { 
+            restID: restID
+    }});
+}
+
+// Query: Get specific item using id
+async function get_by_id(restID, itemID)
+{
+    return await MenuItem.findOne({
+        attributes: [
+            "name",
+            "price",
+            "description",
+            "category"
+        ], where: { 
+            itemID: itemID,
+            restID: restID
+    }});
+}
+
+// Query: Update column: name
+async function change_name(restID, itemID, name)
+{
+    return await MenuItem.update({ 
+        name: name
+    },{ where: { 
+        itemID: itemID, 
+        restID: restID
+    }});
+}
+
+// Query: Update column: price
+async function change_price(restID, itemID, price)
+{
+    return await MenuItem.update({ 
+        price: price
+    },{ where: { 
+        itemID: itemID, 
+        restID: restID
+    }});
+}
+
+// Query: Update column: description
+async function change_description(restID, itemID, desc)
+{
+    return await MenuItem.update({ 
+        description: desc
+    },{ where: { 
+        itemID: itemID, 
+        restID: restID
+    }});
+}
+
+// Query: Update column: category
+async function change_category(restID, itemID, category)
+{
+    return await MenuItem.update({ 
+        category: category
+    },{ where: { 
+        itemID: itemID, 
+        restID: restID
+    }});
+}
+
+// Query: Destroy a menu item
+async function destroy(restID, itemID)
+{
+    return await MenuItem.destroy({
+        where: { 
+            itemID: itemID,
+            restID: restID 
+    }});
+}
+
+export default
+{
+    create,
+    get_by_restaurant,
+    get_by_id,
+    change_name,
+    change_price,
+    change_description,
+    change_category,
+    destroy
+}
--- source/backend/models/Restaurant.js
+++ source/backend/models/Restaurant.js
@@ -9,9 +9,134 @@ export const Restaurant = sequelize.define("Restaurant", {
     userID:      { type: DataTypes.INTEGER, allowNull: false }, // Owner / Foreign key
     name:        { type: DataTypes.STRING,  allowNull: false },
     address:     { type: DataTypes.STRING,  allowNull: false, unique: true },
-    phone:   { type: DataTypes.STRING,  allowNull: false, unique: true },
-    tags:        { type: DataTypes.ARRAY(DataTypes.STRING),  defaultValue: ["no-tags-created"]},
-    description: { type: DataTypes.TEXT },
-    open_hours:  { type: DataTypes.TEXT },
-    logo:        { type: DataTypes.STRING }, // Filepath to image
+    phone:       { type: DataTypes.STRING,  allowNull: false, unique: true },
+    tags:        { type: DataTypes.ARRAY(DataTypes.STRING),  defaultValue: ["no-tags-created"] },
+    description: { type: DataTypes.TEXT, defaultValue: "" },
+    logo:        { type: DataTypes.STRING, defaultValue: "" }, // Filepath to image
 });
+
+// Query: Create a new restaurant
+async function create(ownerID, name, address, phone, tags)
+{
+    try{ // Attempt to create the restaurant
+
+        return await Restaurant.create({
+            userID: ownerID,
+            name: name,
+            address: address,
+            phone: phone,
+            tags: tags
+        });
+
+    }catch{ // Failed to create the restaurant
+
+        throw new Error("Failed to create new restaurant, verify the information provided is correct.");
+    }
+}
+
+// Query: Update column: name
+async function change_name(id, name)
+{
+    return await Restaurant.update({ 
+            name: name 
+        },{ where: { 
+            restID: id 
+    }});
+}
+
+// Query: Update column: address
+async function change_address(id, address)
+{
+    return await Restaurant.update({ 
+            address: address
+        },{ where: { 
+            restID: id 
+    }});
+}
+
+// Query: Update column: phone
+async function change_phone(id, phone)
+{
+    return await Restaurant.update({ 
+            phone: phone
+        },{ where: { 
+            restID: id 
+    }});
+}
+
+// Query: Update column: description
+async function change_description(id, desc)
+{
+    return await Restaurant.update({ 
+            description: desc
+        },{ where: { 
+            restID: id 
+    }});
+}
+
+// Query: Update column: tags
+async function change_tags(id, tags)
+{
+    return await Restaurant.update({ 
+            tags: tags
+        },{ where: { 
+            restID: id 
+    }});
+}
+
+// Query: List of all restaurants
+async function get_all()
+{
+    return await Restaurant.findAll();
+}
+
+// Query: Get restaurant using id
+async function get_by_id(id)
+{
+    return await Restaurant.findOne({ 
+        where: { 
+            restID: parseInt(id)
+    }});
+}
+
+// Query: Get restaurant using owner
+async function get_by_owner(ownerID)
+{
+    return await Restaurant.findOne({ 
+        where: { 
+            userID: parseInt(ownerID) 
+    }});
+}
+
+// Query: Get restaurant using address
+async function get_by_address(address)
+{
+    return await Restaurant.findOne({ 
+        where: { 
+            address: address 
+    }});
+}
+
+// Query: Get restaurant using phone
+async function get_by_phone(phone)
+{
+    return await Restaurant.findOne({ 
+        where: { 
+            phone: phone
+    }});
+}
+
+export default
+{
+    create,
+    change_name,
+    change_address,
+    change_phone,
+    change_description,
+    change_tags,
+    get_all,
+    get_by_id,
+    get_by_owner,
+    get_by_address,
+    get_by_phone
+}
