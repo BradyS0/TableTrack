fe8e89e5ae7513a48c3ed8c98f4d8e466a6bf849
Braydon Schick <113713594+BradyS0@users.noreply.github.com>
2025-11-25 11:55:44 -0600

    105 refactor db calls from routes to models (#109)
    * Queries to data layer
    
    * Fixed test setup data
    
    Test setup data now calls functions rather than the model directly.
    NOTE: Still does not work
    
    * chnaged model function names to be diff from db functions. fixed uncreated var in user router
    
    * chnaged findByPK to get_by_id in schedule router
    
    * changed models to not export model itself. changed price to money as money is parsed to db compatible format
    
    * fixed user integration tests
    
    * fixed restaurant tests
    was returning  old data, now returns updated data
    
    * fixed schedule int tests
    was using infByPK not get_by_id
    
    * make changes recommended by copilot
    
    ---------
    
    Co-authored-by: Matt-Byk <boychu13@myumanitoba.ca>

--- source/backend/models/User.js
+++ source/backend/models/User.js
@@ -2,10 +2,146 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../db.js"; // updated import
 
-export const User = sequelize.define("User", {
+const User = sequelize.define("User", {
     userID: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     first_name: { type: DataTypes.STRING, allowNull: false },
     last_name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
 });
+
+// Query: Create a new user
+async function create_new(first_name, last_name, email, password)
+{
+    try{ // Attempt to create the user
+
+        await User.create({
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: password
+        });
+        return await get_by_email(email);
+
+    }catch{ // Failed to create the user
+
+        throw new Error("Failed to create a new user, verify the information provided is correct.")
+    }
+}
+
+// Query: Match email and password
+async function login(email, password)
+{
+    return await User.findOne({
+        attributes: [
+            'userID',
+            'first_name',
+            'last_name',
+            'email'
+        ], where: {
+            email: email,
+            password: password
+    }});
+}
+
+// Query: Delete a user
+async function destroy_user(userID)
+{
+    return await User.destroy({
+        where: {
+            userID: userID 
+    }});
+}
+
+// Query: Get user using userID
+async function get_by_id(id)
+{
+    return await User.findOne({ 
+        attributes: [
+            'userID',
+            'first_name',
+            'last_name',
+            'email'
+        ], where: {
+            userID: id,
+    }});
+}
+
+// Query: Get user using email
+async function get_by_email(email)
+{
+    return await User.findOne({ 
+        attributes: [
+            'userID',
+            'first_name',
+            'last_name',
+            'email'
+        ], where: {
+            email: email,
+    }});
+}
+
+// Query: Change user firstname
+async function change_firstname(id, name)
+{
+    return await User.update({ 
+            first_name: name 
+        },{ where: { 
+            userID: id
+    }});
+}
+
+// Query: Change user lastname
+async function change_lastname(id, name)
+{
+    return await User.update({ 
+            last_name: name 
+        },{ where: { 
+            userID: id 
+    }});
+}
+
+// Query: Change user email
+async function change_email(id, email)
+{
+    return await User.update({ 
+            email: email
+        },{ where: { 
+            userID: id 
+    }});
+}
+
+// Query: Get a users password
+async function get_password(id)
+{
+    return await User.findOne({ 
+        attributes: [
+            'password'
+        ], where: {
+            userID: id
+    }});
+}
+
+// Query: Change user password
+async function change_password(id, pass)
+{
+    return await User.update({ 
+            password: pass
+        },{ where: { 
+            userID: id 
+    }});
+}
+
+export default
+{
+    create_new,
+    login,
+    destroy_user,
+    get_by_id,
+    get_by_email,
+    change_firstname,
+    change_lastname,
+    change_email,
+    get_password,
+    change_password
+};
--- source/backend/routes/userRouter.js
+++ source/backend/routes/userRouter.js
@@ -1,30 +1,23 @@
 // file partly created using chatGPT
 import express from "express";
-import { User } from "../models/User.js";
+import User from "../models/User.js";
 import UserLogic from "../logic/userLogic.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    const { first_name, last_name, email, password } = req.body;
-    const email_list = await User.findOne({
-        where: { email: email }
-    });
 
+    const { first_name, last_name, email, password } = req.body;
     const valid_params = UserLogic.validate_name(first_name) && UserLogic.validate_name(last_name)
         && UserLogic.validate_email(email) && UserLogic.validate_password(password);
 
+    let email_list = await User.get_by_email(email);
     if (email_list || !valid_params)
         return res.status(400).json({ error: "Invalid parameters" });
 
     try {
         const hashed_password = UserLogic.hash_password(password);
-        const user = await User.create({
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password: hashed_password
-        });
+        const user = await User.create_new(first_name, last_name, email, hashed_password);
         res.status(201).json({
             first_name: user.first_name,
             last_name: user.last_name,
@@ -33,18 +26,12 @@ router.post("/", async (req, res) => {
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
-
 });
 
 router.post("/login", async (req, res) => {
     const { email, password } = req.body;
-    const user = await User.findOne({
-        attributes: ['userID', 'first_name', 'last_name', 'email'],
-        where: {
-            email: email,
-            password: UserLogic.hash_password(password)
-        }
-    });
+    const hashed_password = UserLogic.hash_password(password);
+    const user = await User.login(email, hashed_password);
 
     if (!user)
         return res.status(401).json({ error: "Invalid email or password" });
@@ -56,9 +43,8 @@ router.post("/login", async (req, res) => {
 });
 
 router.delete("/:userID", async (req, res) => {
-    const deleted = await User.destroy({
-        where: { userID: parseInt(req.params.userID) },
-    });
+    const id = parseInt(req.params.userID);
+    const deleted = await User.destroy_user(id);
     if (deleted) res.status(204).send();
     else res.status(404).json({ error: "User not found" });
 });
@@ -69,10 +55,7 @@ router.patch("/change/firstname", async (req, res) => {
     if (!UserLogic.validate_name(first_name))
         return res.status(400).json({ error: "Invalid first name" })
 
-    const updated = await User.update({ first_name: first_name },
-        {
-            where: { userID: userID }
-        });
+    const updated = await User.change_firstname(userID, first_name);
 
     if (updated[0]) {
         //user exists
@@ -86,12 +69,9 @@ router.patch("/change/lastname", async (req, res) => {
     const { userID, last_name } = req.body;
 
     if (!UserLogic.validate_name(last_name))
-        return res.status(400).json({ error: "Invalid last name" })
+        return res.status(400).json({ error: "Invalid last name" });
 
-    const updated = await User.update({ last_name: last_name },
-        {
-            where: { userID: userID }
-        });
+    const updated = await User.change_lastname(userID, last_name);
 
     if (updated[0]) {
         //user exists
@@ -107,13 +87,13 @@ router.patch("/change/email", async (req, res) => {
     if (!UserLogic.validate_email(email))
         return res.status(400).json({ error: "Invalid email syntax" });
 
-    const emailList = await User.findOne({ where: { email: email } });
+    const emailList = await User.get_by_email(email);
 
     if (emailList)
         return res.status(400).json({ error: "Invalid parameter" });
 
     //email is not a duplicate
-    const updated = await User.update({ email: email }, { where: { userID:userID } });
+    const updated = await User.change_email(userID, email);
     if (updated[0]) {
         res.status(200).json({ message: "Email updated" });
     } else {
@@ -126,24 +106,19 @@ router.patch("/change/password", async (req, res) => {
     const { userID, old_password, new_password } = req.body;
     try {
         //account password stored in database
-        const user = await User.findAll({
-            attributes: ['password'],
-            where: {
-                userID: userID
-            }
-        });
+        const user = await User.get_password(userID);
 
         const old_hashed = UserLogic.hash_password(old_password);
 
         if (!UserLogic.validate_password(old_password)) {
             res.status(400).json({ message: "Old password is invalid" });
-        } else if (user[0].password !== old_hashed) {
+        } else if (user.password !== old_hashed) {
             res.status(401).json({ message: "Passwords do not match" });
         } else if (!UserLogic.validate_password(new_password)) {
             res.status(400).json({ message: "New password is invalid" });
         } else {
             const new_hashed = UserLogic.hash_password(new_password);
-            await User.update({ password: new_hashed }, { where: { userID } });
+            await User.change_password(userID, new_hashed);
 
             //already checked if user exists
             res.status(200).json({ message: "Password updated" });
