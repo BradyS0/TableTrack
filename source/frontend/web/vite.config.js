dc3126d234be0df91fa3fa0f57f29c291a705ea2
Braydon Schick <starwarsboi77@gmail.com>
2025-11-29 16:50:02 -0600

    made website into vite project

---
+++ source/frontend/web/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// Change API URL if needed (your Render deployment URL)
+const API_URL = process.env.VITE_API_URL || "http://localhost:5500"
+
+export default defineConfig({
+  define: {
+    __API_URL__: JSON.stringify(API_URL)
+  }
+})
