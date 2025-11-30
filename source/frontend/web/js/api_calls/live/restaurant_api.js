14fef0ede2cd1350b376ac46ed786b16ded4e792
BradyS0 <starwarsboi77@gmail.com>
2025-11-30 11:59:19 -0600

    remove unused variable

--- source/frontend/web/js/api_calls/live/restaurant_api.js
+++ source/frontend/web/js/api_calls/live/restaurant_api.js
@@ -1,5 +1,5 @@
 import { getRandomRating } from "../mock/restaurant_api.js";
-const API_URL = import.meta.env.VITE_API_URL || __API_URL__;
+const API_URL = import.meta.env.VITE_API_URL;
 const API = `${API_URL}/v1/restaurant`;
 
 async function getRestaurants() {
