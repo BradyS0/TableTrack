83704ba3b06bac166a64ed45bcc7188616cb31dd
Arsalan Siddiqui <arsalan2708@gmail.com>
2025-11-30 19:00:51 -0600

    created Reservation Model and added to index.js of the models

---
+++ source/backend/db/models/Reservation.js
@@ -0,0 +1,62 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db.js";
+
+export const Reservation = sequelize.define("Reservation",
+  {
+    reservationID: {
+      type: DataTypes.STRING(7),
+      allowNull: false,
+      unique: true,
+      defaultValue: sequelize.literal("substring(md5(random()::text), 1, 7)"),
+    },
+
+    restID:  { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        allowNull : false,
+        references: { model: 'Restaurants', key: "restID" },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+     },
+
+    tableID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    userID:  { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        allowNull: false,
+        references: { model: 'User', key: "userID" },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+     },
+
+    dateStamp: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    guestAmount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1, 
+      validate: {min: 1},
+    },
+
+    status: {
+      type: DataTypes.ENUM("ok", "needs-attention"),
+      allowNull: false,
+      defaultValue: "ok",
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["restID", "tableID", "dateStamp"]
+      },
+    ],
+  }
+);
--- source/backend/db/models/index.js
+++ source/backend/db/models/index.js
@@ -4,11 +4,14 @@ import { User } from "./User.js";
 import { Restaurant } from "./Restaurant.js";
 import { Schedule } from "./Schedule.js";
 import { MenuItem } from "./MenuItem.js";
+import {Reservation} from "./Reservation.js"
 
 Restaurant.belongsTo(User, { foreignKey: 'userID', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
 MenuItem.belongsTo(Restaurant, { foreignKey: 'restID', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
 Schedule.belongsTo(Restaurant, { foreignKey: 'restID', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
 
+Reservation.belongsTo(Restaurant, { foreignKey: 'restID', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
+Reservation.belongsTo(User, { foreignKey: 'userID', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
 // Export models and sequelize so other parts of the app can import from a single place
 export { sequelize, DataTypes, User, Restaurant, Schedule, MenuItem };
 
